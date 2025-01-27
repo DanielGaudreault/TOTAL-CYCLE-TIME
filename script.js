27aa45ed6f9b93de17f0ea592c7d8370112409d6
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-01-27 12:20:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ document.getElementById('upload-form').addEventListener('submit', function(event
     })
     .then(response => {
         console.log('Response status:', response.status);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
         return response.json();
     })
     .then(data => {
